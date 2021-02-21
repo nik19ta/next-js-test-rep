6d9d814c019d447b60395200d8cb11e372ed8937
nik19ta <nikhvatov19@gmail.com>
2021-02-21 15:27:55 +0300

    добавил фавикон

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,9 @@ export default function App() {
         <div>
             <Head>
                 <title>Next App</title>
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
                 <link href="/main.css" />
             </Head>
 
